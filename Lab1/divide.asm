593a78bbcd02fa2c013282517c5479df56fa706f
Alex Painter <32502264+alexpainter3@users.noreply.github.com>
2020-01-14 03:20:31 -0800

    Add files via upload

---
+++ Lab1/divide.asm
@@ -0,0 +1,143 @@
+#Alex Painter
+#Section: 5
+
+#Java interpretation:
+#int[2] divide(int dividendHigh, int dividendLow, int divisor){
+#	int mask = 1;
+#	while (mask != divisor){
+#		mask << 1;
+#		dividendLow >> 1;
+#		if ((dividendHigh & 0x1) != 0){
+#			dividendLow = dividendLow | 0x80000000;
+#		}
+#	}
+#	return [dividendHigh, dividendLow];
+#}
+
+
+#global declarations
+.globl welcome 
+.globl prompt_dividend_high
+.globl prompt_dividend_low
+.globl prompt_divisor
+.globl result_upper_32
+.globl result_lower_32
+
+#Data
+.data
+
+welcome: 
+	.asciiz "This program divides two numbers"					#33
+
+prompt_dividend_high:
+	.asciiz "\n\nEnter the upper 32 bits of the dividend: "		#45
+
+prompt_dividend_low:
+	.asciiz "\n\nEnter the lower 32 bits of the dividend: "		#45
+
+prompt_divisor:
+	.asciiz "\n\nEnter a 31 bit divisor: "						#28
+
+result_upper_32:
+	.asciiz "\n\nUpper 32 bits of result: "						#29
+
+result_lower_32:
+	.asciiz "\n\nLower 32 bits of result: "	 					#29
+
+#Text Area (i.e. instructions)
+.text
+
+main:
+	#load welcome message
+	ori     $v0, $0, 4
+	lui     $a0, 0x1001
+	syscall	
+
+	#prompt user to enter high dividend
+	ori     $v0, $0, 4	
+	lui     $a0, 0x1001
+	ori     $a0, $a0, 0x21
+	syscall
+
+	#read and store user input
+	ori     $v0, $0, 5
+	syscall
+
+	#clear register and store number
+	ori     $s0, $0, 0
+	addu    $s0, $v0, $s0
+
+
+	#prompt user to enter low dividend
+	ori     $v0, $0, 4	
+	lui     $a0, 0x1001
+	ori     $a0, $a0, 0x4E
+	syscall
+
+	#read and store user input
+	ori     $v0, $0, 5
+	syscall
+
+	#clear register and store number
+	ori     $s1, $0, 0
+	addu    $s1, $v0, $s1
+
+	#prompt user to enter divisor
+	ori     $v0, $0, 4	
+	lui     $a0, 0x1001
+	ori     $a0, $a0, 0x7A
+	syscall
+
+	#read and store user input
+	ori     $v0, $0, 5
+	syscall
+
+	#clear register and store number
+	ori     $s2, $0, 0
+	addu    $s2, $v0, $s2
+
+	#Logic
+	#initialize register for mask
+	addi	$s3, $s3, 1
+
+loop:
+	beq		$s2, $s3, print
+	sll     $s3, $s3, 1
+	srl 	$s1, $s1, 1
+	andi	$s4, $s0, 1
+	beq		$s4, $0, shift_upper
+	ori		$s5, $0, 1
+	sll     $s5, $s5, 31
+	or   	$s1, $s1, $s5
+
+shift_upper:
+	srl 	$s0, $s0, 1 
+	j 		loop
+
+print:
+	#display results text to user
+	ori     $v0, $0, 4	
+	lui     $a0, 0x1001
+	ori     $a0, $a0, 0x95
+	syscall
+
+	#print result upper 32 bits
+	ori     $v0, $0, 1
+	add 	$a0, $s0, $0
+	syscall	
+
+
+	#display results text to user
+	ori     $v0, $0, 4	
+	lui     $a0, 0x1001
+	ori     $a0, $a0, 0xB0
+	syscall
+
+	#print result upper 32 bits
+	ori     $v0, $0, 1
+	add 	$a0, $s1, $0
+	syscall	
+
+	# Exit (load 10 into $v0)
+	ori     $v0, $0, 10
+	syscall
---
+++ Lab1/mod.asm
@@ -0,0 +1,87 @@
+#Alex Painter
+#Section: 5
+
+#Java interpretation:
+#int mod(int num, int div){
+#	int temp = div - 1;
+#	return temp & num;
+#}
+
+#global declarations
+.globl welcome
+.globl prompt_num
+.globl prompt_div
+.globl result
+
+#Data
+.data
+
+welcome: 
+	.asciiz "This program performs a modulus operation\n\n"
+
+prompt_num:
+	.asciiz "Enter an integer (number): "
+
+prompt_div:
+	.asciiz "\nEnter an integer (divisor): "
+
+result:
+	.asciiz "\nModulus: "
+	 
+#Text Area (i.e. instructions)
+.text
+
+main:
+	#load welcome message
+	ori     $v0, $0, 4
+	lui     $a0, 0x1001
+	syscall	
+	
+	#prompt user to enter number
+	ori     $v0, $0, 4	
+	lui     $a0, 0x1001
+	ori     $a0, $a0, 0x2C
+	syscall
+
+	#read and store user input
+	ori     $v0, $0, 5
+	syscall
+
+	#clear register for number and divisor
+	ori     $s0, $0, 0	
+	ori     $s1, $0, 0
+
+	# Store in register s0
+	addu    $s0, $v0, $s0
+		
+	#prompt user to enter divisor
+	ori     $v0, $0, 4	
+	lui     $a0, 0x1001
+	ori     $a0, $a0, 0x48
+	syscall
+
+	#read and store user input
+	ori     $v0, $0, 5
+	syscall
+
+	# Store in register s1
+	addu    $s1, $v0, $s1
+
+	#logic
+	subu	$s1, $s1, 1
+	and 	$s0, $s0, $s1
+
+	#display results text to user
+	ori     $v0, $0, 4	
+	lui     $a0, 0x1001
+	ori     $a0, $a0, 0x66
+	syscall
+
+	#print result
+	ori     $v0, $0, 1
+	add 	$a0, $s0, $0
+	syscall	
+
+	# Exit (load 10 into $v0)
+	ori     $v0, $0, 10
+	syscall
